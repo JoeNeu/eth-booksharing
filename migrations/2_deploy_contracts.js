11d101067dd1f01ed977e62255d111a6756f7436
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2021-02-11 14:58:26 +0100

    add deployment for UserController

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const UserDatabase = artifacts.require('UserDatabase.sol');
+const UserController = artifacts.require('UserController.sol');
 
 module.exports = async function(deployer,network,accounts) {
-    deployer.deploy(UserDatabase);
+    deployer.deploy(UserDatabase).then(function (userDB) {
+        return deployer.deploy(UserController, userDB.address);
+    })
 };
