b274084b3b56a12b03d5963ae8892926ed1782c4
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2021-02-12 14:43:17 +0100

    adjust deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,16 @@
 const UserController = artifacts.require('UserController.sol');
 const UserDatabase = artifacts.require('UserDatabase.sol');
-const BookShelfController = artifacts.require('BookShelfController.sol');
+const BookController = artifacts.require('BookController.sol');
 const BookDatabase = artifacts.require('BookDatabase.sol');
-const BookShelfEvents = artifacts.require('BookShelfEvents.sol');
 
 module.exports = async function(deployer,network,accounts) {
     deployer.deploy(UserDatabase).then( function(userDB) {
         return deployer.deploy(UserController, userDB.address).then( function (userController) {
-            return userDB.setAccessor(userController.address);
-        })
-    })
-    deployer.deploy(BookShelfEvents).then( function(bookEvents) {
-        return deployer.deploy(BookDatabase).then( function(bookDB) {
-            return deployer.deploy(BookShelfController, bookDB.address, bookEvents.address).then( function (bookController) {
-                bookEvents.setAccessor(bookController.address);
-                return bookDB.setAccessor(bookController.address);
+            userDB.setAccessor(userController.address);
+            return deployer.deploy(BookDatabase).then( function(bookDB) {
+                return deployer.deploy(BookController, bookDB.address, userController.address).then( function (bookController) {
+                    return bookDB.setAccessor(bookController.address);
+                })
             })
         })
     })
